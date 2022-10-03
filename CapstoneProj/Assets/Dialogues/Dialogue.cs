3b72b73dea0e453f02457fe9584d8ff9e4df6bff
Twk-Alymor <hersh.aviles@gmail.com>
2022-10-03 20:55:15 +0800

    Updated Dialogue Editor (Not finished)

--- CapstoneProj/Assets/Dialogues/Dialogue.cs
+++ CapstoneProj/Assets/Dialogues/Dialogue.cs
@@ -10,6 +10,19 @@ namespace First.Dialogue
     public class Dialogue : ScriptableObject
     {
         [SerializeField]
-        DialogueNode[] nodes;
+        List<DialogueNode> nodes;
+
+        // Only included if running the editor
+#if UNITY_EDITOR
+        private void Awake() {
+            if (nodes.Count == 0)
+            {
+                nodes.Add(new DialogueNode());
+            }
+        }
+#endif
+        public IEnumerable<DialogueNode> GetAllNodes() {
+            return nodes;
+        }
     }
 }
--- CapstoneProj/Assets/Dialogues/Editor/DialogueEditor.cs
+++ CapstoneProj/Assets/Dialogues/Editor/DialogueEditor.cs
@@ -8,6 +8,9 @@ namespace First.Dialogue.Editor
 {
     public class DialogueEditor : EditorWindow
     {
+        Dialogue selectedDialogue = null;
+        
+
         // Para magkaroon ng option sa Window
         [MenuItem("Window/Dialogue Editor")]
         public static void showEditorWindow()
@@ -25,6 +28,46 @@ namespace First.Dialogue.Editor
             }
             
             return false;
+        }
+
+        private void OnEnable() 
+        {
+            Selection.selectionChanged += OnSelectionChanged;
+        }
+
+        // Updates the selected dialogue in Dialogue Editor
+        private void OnSelectionChanged() {
+            Dialogue newDialogue = Selection.activeObject as Dialogue;
+            if (newDialogue != null)
+            {
+                selectedDialogue = newDialogue;
+                Repaint();
+            }
+        }
+
+        private void OnGUI() {
+            if (selectedDialogue == null) {
+                EditorGUILayout.LabelField("No Dialogue Selected.");
+            }
+            else {
+                
+                foreach (DialogueNode node in selectedDialogue.GetAllNodes()) {
+                    EditorGUI.BeginChangeCheck();
+
+                    EditorGUILayout.LabelField("Node:");
+                    string newText = EditorGUILayout.TextField(node.text);
+                    string newUniqueID = EditorGUILayout.TextField(node.uniqueID);
+
+                    if (EditorGUI.EndChangeCheck()) {
+                        Undo.RecordObject(selectedDialogue, "Update Dialogue Text");
+                        node.text = newText;
+                    }
+                    
+                }
+            }
+
+            
+            
         }
     }
 }
